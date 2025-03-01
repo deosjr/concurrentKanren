27018113bb690a2890590915b8a7ab1ce6af5db8
Sjoerd Dost <deosjr@gmail.com>
2025-03-01 10:30:17 +0100

    rewrite without worker pool but syncing streams

--- main.go
+++ main.go
@@ -4,13 +4,10 @@ import (
     "context"
     "fmt"
     "strings"
-    "time"
+    //"time"
 )
 
 func main() {
-    defer stopWorkers()
-    startWorkers(100)
-
 /*
     p := pair{variable(1), pair{number(2), pair{variable(3), emptylist}}}
     fmt.Println(p.display())
@@ -23,36 +20,36 @@ func main() {
     str := g(emptystate)
     fmt.Println(takeAll(str))
 
-    g = callfresh(func(q expression) goal { return disj(equalo(q, number(5)), equalo(q, number(6))) })
-    str = g(emptystate)
+    g := callfresh(func(q expression) goal { return disj(equalo(q, number(5)), equalo(q, number(6))) })
+    str := g(emptystate)
     fmt.Println(takeAll(str))
 
-    // TODO: takeN isn't closing the stream, so workers are forever producing even when we stopped reading
-    // this will impact performance of the next run!
-    // Idiomatic way to solve this is using context cancellation
-    g = callfresh(func(x expression) goal { return fives(x) })
-    str = g(emptystate)
+    g := callfresh(func(x expression) goal { return fives(x) })
+    str := g(emptystate)
     fmt.Println(takeN(3, str))
 
-    //g = callfresh(func(x expression) goal { return disj(fives(x), disj(sixes(x), sevens(x))) })
-    //str = g(emptystate)
-    //fmt.Println(takeN(9, str))
+    g := callfresh(func(x expression) goal { return disj(fives(x), disj(sixes(x), sevens(x))) })
+    str := g(emptystate)
+    fmt.Println(takeN(9, str))
 
-    //g = callfresh(func(x expression) goal { return disj_plus(fives(x), sixes(x), sevens(x)) })
-    //str = g(emptystate)
-    //fmt.Println(takeN(9, str))
+    g := callfresh(func(x expression) goal { return disj_plus(fives(x), sixes(x), sevens(x)) })
+    str := g(emptystate)
+    fmt.Println(takeN(9, str))
 
     g := callfresh(func(x expression) goal { return disj_conc(fives(x), sixes(x), sevens(x)) })
     str := g(emptystate)
     fmt.Println(takeN(9, str))
+*/
 
     g := callfresh(func(x expression) goal { return disj_conc(nevero(), fives(x), sixes(x), sevens(x)) })
     str := g(emptystate)
     fmt.Println(takeN(9, str))
-*/
+
+/*
     g := callfresh(func(x expression) goal { return callfresh(func(y expression) goal { return conj(equalo(x,number(5)), equalo(y,number(6))) }) })
     str := g(emptystate)
     fmt.Println(takeAll(str))
+*/
 }
 
 type expression interface {
@@ -180,76 +177,46 @@ var emptystate = state{
 }
 
 type stream struct {
-    ch chan state
+    in  chan bool
+    out chan state
     ctx context.Context
 }
 
 func newStream() stream {
-    ch := make(chan state, 1)
-    return stream{ch:ch, ctx:nil}
+    in := make(chan bool, 1)
+    out := make(chan state, 1)
+    return stream{in:in, out:out, ctx:nil}
 }
 
 type goal func(state) stream
 
-type work func()
-
-var workpool chan work
-
-func startWorkers(numWorkers int) {
-    // TODO: does the buffer grow exponentially and thus potentially block?
-    workpool = make(chan work, numWorkers * 100)
-    for i:=0; i<numWorkers; i++ {
-        go worker()
-    }
-}
-
-func stopWorkers() {
-    close(workpool)
-}
-
-// worker takes the role of pull and take
-func worker() {
-    for f := range workpool {
-        f()
-    }
-}
-
-// TODO: without the timeout we can reach deadlock when all workers are trying to
-// forward onto or read from streams that aren't making progress.
-// There must be a better solution here (increasing workers helps but doesn't scale)
-func forward(ch chan state, str stream) {
-    select {
-    case st, ok := <- str.ch:
-        if !ok {
-            close(ch)
-            return
+// link two streams: send in from parent to child, out from child to parent
+func link(parent, child stream) {
+    go func() {
+        for b := range parent.in {
+            child.in <- b
         }
-        ch <- st
-        workpool <- func() { forward(ch, str) }
-    case <-time.After(1*time.Millisecond):
-        workpool <- func() { forward(ch, str) }
-    }
-/*
-    st, ok := <-str.ch
-    if !ok {
-        close(ch)
-        return
-    }
-    ch <- st
-    workpool <- func() { forward(ch, str) }
-*/
+    }()
+    go func() {
+        for st := range child.out {
+            parent.out <- st
+        }
+        close(parent.out)
+    }()
 }
 
-// equalo is the only goal that doesnt queue work
 func equalo(u, v expression) goal {
     //time.Sleep(1*time.Second)
     return func(st state) stream {
         str := newStream()
-        s, ok := st.sub.unify(u, v)
-        if ok {
-            str.ch <- state{ sub:s, vc:st.vc }
-        }
-        close(str.ch)
+        go func() {
+            <- str.in
+            s, ok := st.sub.unify(u, v)
+            if ok {
+                str.out <- state{ sub:s, vc:st.vc }
+            }
+            close(str.out)
+        }()
         return str
     }
 }
@@ -265,79 +232,91 @@ func callfresh(f func (x expression) goal) goal {
 func disj(g1, g2 goal) goal {
     return func(st state) stream {
         str := newStream()
-        str1 := g1(st)
-        str2 := g2(st)
-        workpool <- func() {
-            mplus(str.ch, str1, str2)
-        }
+        go func() {
+            mplus(str, g1(st), g2(st))
+        }()
         return str
     }
 }
 
-func mplus(ch chan state, str1, str2 stream) {
-    v, ok := <-str1.ch
+func mplus(str, str1, str2 stream) {
+    <-str.in
+    str1.in <- true
+    v, ok := <-str1.out
     if !ok {
-        workpool <- func() { forward(ch, str2) }
+        str2.in <- true
+        link(str, str2)
         return
     }
-    if !v.delayed {
-        ch <- v
+    if v.delayed {
+        str.in <- true
+    } else {
+        str.out <- v
     }
-    workpool <- func() { mplus(ch, str2, str1) }
+    mplus(str, str2, str1)
 }
 
 func conj(g1, g2 goal) goal {
     return func(st state) stream {
         str := newStream()
-        str1 := g1(st)
-        workpool <- func() {
-            bind(str.ch, str1, g2)
-        }
+        go func() {
+            bind(str, g1(st), g2)
+        }()
         return str
     }
 }
 
-func bind(ch chan state, str stream, g goal) {
-    v, ok := <-str.ch
+func bind(str, str1 stream, g goal) {
+    str1.in <- true
+    v, ok := <-str1.out
     if !ok {
-        close(ch)
+        close(str.out)
         return
     }
     if v.delayed {
-        workpool <- func() { bind(ch, str, g) }
+        bind(str, str1, g)
         return
     }
     bstr := newStream()
-    workpool <- func() { bind(bstr.ch, str, g) }
-    workpool <- func() { mplus(ch, g(v), bstr) }
+    go func() {
+        bind(bstr, str1, g)
+    }()
+    mplus(str, g(v), bstr)
 }
 
 func takeAll(str stream) []state {
     states := []state{}
-    for st := range str.ch {
+    str.in <- true
+    for st := range str.out {
         states = append(states, st)
+        str.in <- true
     }
+    close(str.in)
     return states
 }
 
 func takeN(n int, str stream) []state {
     states := []state{}
+    str.in <- true
     for i:=0; i<n; i++ {
-        st, ok := <-str.ch
+        st, ok := <-str.out
         if !ok {
             return states
         }
         states = append(states, st)
+        str.in <- true
     }
+    close(str.in)
     return states
 }
 
-// NOTE: creates a new stream for each delay, probably not that efficient
 func delay(f func() goal) goal {
     return func(st state) stream {
         str := newStream()
-        str.ch <- state{delayed:true}
-        workpool <- func() { forward(str.ch, f()(st)) }
+        go func() {
+            str.out <- state{delayed:true}
+            link(str, f()(st))
+        }()
         return str
     }
 }
@@ -369,23 +348,12 @@ func disj_conc(goals ...goal) goal {
         for _, g := range goals {
             streams = append(streams, g(st))
         }
-        var f func()
-        f = func() {
-            if len(buffer) > 0 {
-                str.ch <- buffer[0]
-                buffer = buffer[1:]
-                workpool <- f
-                return
-            }
-            if len(streams) == 0 {
-                close(str.ch)
-                return
-            }
-            // refill the buffer
+        refillBuffer := func() {
             buffer = []state{}
             unproductive := map[int]struct{}{}
-            for i, in := range streams {
-                x, ok := <- in.ch
+            for i, s := range streams {
+                s.in <- true
+                x, ok := <- s.out
                 if !ok {
                     unproductive[i] = struct{}{}
                     continue
@@ -403,9 +371,26 @@ func disj_conc(goals ...goal) goal {
                 active = append(active, s)
             }
             streams = active
-            workpool <- f
         }
-        workpool <- f
+        var f func()
+        f = func() {
+            <-str.in
+            for len(buffer) == 0 && len(streams) > 0 {
+                refillBuffer()
+            }
+            if len(buffer) > 0 {
+                str.out <- buffer[0]
+                buffer = buffer[1:]
+                f()
+                return
+            }
+            if len(streams) == 0 {
+                close(str.out)
+                return
+            }
+            panic("should never happen")
+        }
+        go f()
         return str
     }
 }
