48c70574a9921827fe84602e9f82b0c88bcf434f
Sjoerd Dost <deosjr@gmail.com>
2025-08-30 15:25:27 +0200

    pq is overly fancy

--- worker.go
+++ worker.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"container/heap"
-	"math/rand/v2"
 	"sync"
 )
 
@@ -34,10 +32,9 @@ var (
 	suspendedReq = map[stream]reqFn{}
 	inbox        = map[stream]Message{}
 	suspendedRec = map[stream]receiveFn{}
-	pq = NewPriorityQueue()
-	pqIn = make(chan any, 1)
-	pqOut = make(chan any)
-	pqMut       sync.Mutex
+	qIn = make(chan any, 100)
+	qOut = make(chan any, 100)
+	q = []any{}
 )
 
 func startWorkers() *sync.WaitGroup {
@@ -48,7 +45,7 @@ func startWorkers() *sync.WaitGroup {
 	for range numWorkers {
 		ch <- struct{}{}
 	}
-	go managePriorityQueue(pq)
+	go manageQueue(q)
 	go manageWorkers(ch, &wg)
 	return &wg
 }
@@ -76,10 +73,9 @@ func awaitWorkers(wg *sync.WaitGroup) {
 func work(ch chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
-		pqMut.Lock()
-		pqIn <- nil
-		w := <-pqOut
-		pqMut.Unlock()
+		// doesnt matter if req/resp gets swapped between workers!
+		qIn <- nil
+		w := <-qOut
 		if w == nil {
 			ch <- struct{}{}
 			return
@@ -107,7 +103,7 @@ func registerRequest(str stream, reqFn reqFn) {
 		return
 	}
 	req.fn = reqFn
-	pqIn <- req
+	qIn <- req
 }
 
 // make a request for more work. suspend if no one is waiting for requests
@@ -140,7 +136,7 @@ func registerReceive(str stream, recFn receiveFn) {
 	if !ok {
 		return
 	}
-	pqIn <- receiveWork{
+	qIn <- receiveWork{
 		msg: msg,
 		fn:  recFn,
 	}
@@ -163,83 +159,20 @@ func send(receiver stream, msg Message) {
 	fn(msg)
 }
 
-// heap implementation from https://pkg.go.dev/container/heap
-// An Item is something we manage in a priority queue.
-type Item struct {
-	value    any    // The value of the item; arbitrary.
-	priority int    // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
-}
-
-// A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].priority > pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x any) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // don't stop the GC from reclaiming the item eventually
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
-func NewPriorityQueue() *PriorityQueue {
-	var pq PriorityQueue
-	heap.Init(&pq)
-	return &pq
-}
-
-func Push(pq *PriorityQueue, work any) {
-	item := &Item{
-		value:    work,
-		priority: rand.Int(),
-	}
-	heap.Push(pq, item)
-}
-
-func Pop(pq *PriorityQueue) (any, bool) {
-	if pq.Len() == 0 {
-		return nil, false
-	}
-	item := heap.Pop(pq).(*Item)
-	return item.value, true
-}
-
-func managePriorityQueue(pq *PriorityQueue) {
+func manageQueue(q []any) {
 	for {
-		req := <-pqIn
+		req := <-qIn
 		if req == nil {
 			// request for item
-			v, ok := Pop(pq)
-			if !ok {
-				pqOut <- nil
+			if len(q) == 0 {
+				qOut <- nil
 				continue
 			}
-			pqOut <- v
+			v := q[0]
+			q = q[1:]
+			qOut <- v
 			continue
 		}
-		Push(pq, req)
+		q = append(q, req)
 	}
 }
